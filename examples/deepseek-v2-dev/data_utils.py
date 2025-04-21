9f504f52e1e13cb778fb12edb60b5c4cc9b14238
Luo Yuqi <luo-yq23@mails.tsinghua.edu.cn>
2025-04-21 19:51:35 +0800

    feat: enhance DistributedIteratorDataLoader and add checkpointing functionality in training script

--- examples/deepseek-v2-dev/data_utils.py
+++ examples/deepseek-v2-dev/data_utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import mindspore as ms
 import bmtrain_mindspore as bms
 
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
 from typing import Iterable, List
 from itertools import tee
 from mindnlp.transformers import PreTrainedTokenizerBase
@@ -25,23 +25,32 @@ def mark_last(iterable):
 
 class DistributedIteratorDataLoader:
     def __init__(self,
-            dataset: Iterable[str],
+            dataset: Dataset,
             tokenizer: PreTrainedTokenizerBase,
             batch_size: int,
             max_length: int,
             mean_compression_ratio: float = 1.0,
+            start_index: int = 0,
         ):
-        self.dataset = dataset
+        self.dataset = dataset.shard(
+            num_shards=bms.world_size(),
+            index=bms.rank(),
+        )
+        self.dataset = self.dataset.select(
+            range(start_index, len(self.dataset))
+        )
+        print('dataset')
+        print(self.dataset)
         self.batch_size = batch_size
         self.tokenizer = tokenizer
         self.max_length = max_length
         self.mean_compression_ratio = mean_compression_ratio
+        self.start_index = start_index
 
         # Calculate chunk size of text based on max_length and mean_compression_ratio
         self.chunk_size = int(self.max_length * self.mean_compression_ratio)
-        self.rank = bms.rank()
 
-    def chunk_text_by_length(self, text: str):
+    def _chunk_text_by_length(self, text: str):
         """
         Splits the text into smaller chunks based on the max_length and mean_compression_ratio.
         """
@@ -58,8 +67,9 @@ class DistributedIteratorDataLoader:
         Concatenate the text from the dataset into chunks of max_length.
         """
         concat_text = ''
-        for text in self.dataset:
-            for chunk in self.chunk_text_by_length(text):
+        for self.start_index, data_dict in enumerate(self.dataset, start=self.start_index):
+            text = data_dict['text']
+            for chunk in self._chunk_text_by_length(text):
                 if len(concat_text) > 0:
                     concat_text += SENTENCE_BOUNDARY_MARKER
                 concat_text += chunk
@@ -72,22 +82,10 @@ class DistributedIteratorDataLoader:
         if len(concat_text) > 0:
             yield concat_text
 
-    def _filter_by_rank(self, iterable: Iterable[str]):
-        """
-        Filters the iterable by rank.
-        """
-        for i, item in enumerate(iterable):
-            if i % bms.world_size() == self.rank:
-                yield item
-
     def __iter__(self):
         all_reduce = ops.AllReduce()
         batch = []
-        for text, is_last in mark_last(
-            self._filter_by_rank(
-                self._generate_concated_text()
-            )
-        ):
+        for text, is_last in mark_last(self._generate_concated_text()):
             batch.append(text)
             if len(batch) == self.batch_size:
                 yield self.tokenizer.batch_encode_plus(
--- examples/deepseek-v2-dev/train.py
+++ examples/deepseek-v2-dev/train.py
@@ -1,4 +1,8 @@
 import os
+os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+
+import json
+import shutil
 import numpy as np
 import mindspore as ms
 import bmtrain_mindspore as bms
@@ -6,7 +10,7 @@ import bmtrain_mindspore as bms
 from time import time
 from typing import Iterable
 from datasets import load_dataset
-from mindspore import mint, Tensor, Parameter, ops
+from mindspore import mint, Tensor, Parameter, ops, nn
 from mindnlp.transformers import PreTrainedTokenizerFast, AutoTokenizer
 from bmtrain_mindspore.model_center.model.deepseek_v2 import DeepseekV2
 from bmtrain_mindspore.utils import Timer
@@ -15,46 +19,107 @@ from bmtrain_mindspore.model_center.lr_scheduler import WarmupStableDecayLRSched
 from data_utils import DistributedIteratorDataLoader
 
 LLM_PATH = '/root/thunlp/data/DeepSeek-V2-Lite-ms'
+CHECKPOINT_ROOT = '/root/thunlp/data/moe_checkpoints'
 
-def iter_func(text_items) -> Iterable[str]:
-    cnt = 0
-    for item in text_items:
-        yield item['text']
-        cnt += 1
-        if cnt > 5e3: break
+def save_checkpoint(
+    iter: int,
+    model: DeepseekV2,
+    optimizer: nn.Cell,
+    data_loader: DistributedIteratorDataLoader,
+    num_token_passed: int,
+):
+    save_path = os.path.join(CHECKPOINT_ROOT, f"iter_{iter}")
+    rank = bms.rank()
+    os.makedirs(save_path, exist_ok=True)
+    shutil.copy(
+        os.path.join(LLM_PATH, "config.json"),
+        os.path.join(save_path, "config.json"),
+    )
+    bms.save(
+        model,
+        os.path.join(save_path, "mindspore_model.safetensors"),
+    )
+    ms.save_checkpoint(
+        optimizer,
+        os.path.join(save_path, f'optimizer_rank{rank}.ckpt'),
+    )
+    metadata = {
+        "iter": iter,
+        "data_loader_start_index": data_loader.start_index,
+        "num_token_passed": num_token_passed,
+    }
+    with open(os.path.join(save_path, f'metadata_rank{rank}.json'), "w") as f:
+        json.dump(metadata, f)
+    print(f"Checkpoint saved at {save_path}")
 
-def train():
+def init_from_checkpoint(
+    checkpoint_iter: int = None,
+):
     tokenizer: PreTrainedTokenizerFast = AutoTokenizer.from_pretrained(LLM_PATH)
     tokenizer.pad_token = tokenizer.eos_token
+    rank = bms.rank()
 
+    if checkpoint_iter is None:
+        checkpoint_iter = -1
+        data_loader_start_index = 0
+        checkpoint_path = LLM_PATH
+        num_token_passed = 0
+    else:
+        checkpoint_path = os.path.join(CHECKPOINT_ROOT, f"iter_{checkpoint_iter}")
+        with open(os.path.join(checkpoint_path, f'metadata_rank{rank}.json'), "r") as f:
+            metadata = json.load(f)
+        data_loader_start_index = metadata["data_loader_start_index"]
+        num_token_passed = metadata["num_token_passed"]
+
+    # Load the dataset
     train_loader = DistributedIteratorDataLoader(
-        dataset=iter_func(load_dataset('venketh/SlimPajama-62B', split='train')),
-        batch_size=1,
+        dataset=load_dataset('venketh/SlimPajama-62B', split='train'),
         tokenizer=tokenizer,
-
-        #max_length=4096 + 1, # +1 for next token prediction
-        max_length=1024 + 1, # +1 for next token prediction
-
+        batch_size=6,
+        max_length=4096 + 1, # +1 for next token prediction
         mean_compression_ratio=4.23, # the mean compression ratio of this dataset computed by me
+        start_index=data_loader_start_index,
     )
 
-    model = DeepseekV2.from_pretrained(LLM_PATH)
+    model = DeepseekV2.from_pretrained(checkpoint_path)
     model.set_train(True)
 
-    print('mem_loc before', ms.runtime.memory_allocated() / (2**30))
-
     lr = 1e-5
-    optimizer = mint.optim.Adam(model.trainable_params())
+    optimizer = mint.optim.Adam(
+        model.trainable_params(),
+        betas=(0.9, 0.95),
+        weight_decay=0.1,
+    )
+    # this code prevents scheduler from raising error when loading checkpoint
+    for pg in optimizer.param_groups:
+        pg.setdefault('initial_lr', 1e9)
+    # end
+    opt_path = os.path.join(checkpoint_path, f'optimizer_rank{rank}.ckpt')
+    if os.path.exists(opt_path):
+        print(f"Loading optimizer from {opt_path}")
+        ms.load_checkpoint(opt_path, optimizer)
+    else:
+        print(f"Optimizer checkpoint not found at {opt_path}, using default optimizer")
+
     scheduler = WarmupStableDecayLRScheduler(
         optimizer=optimizer,
+        last_epoch=checkpoint_iter,
         warmup_steps=100,
         decay_start=float('inf'),
         total_iters=float('inf'),
         lr=lr,
     )
-    loss_func = mint.nn.CrossEntropyLoss()
 
-    print('mem_loc', ms.runtime.memory_allocated() / (2**30))
+    print(f"Checkpoint loaded from {checkpoint_path}")
+    start_iter = checkpoint_iter + 1
+
+    return start_iter, model, optimizer, train_loader, scheduler, num_token_passed
+
+def train():
+    start_iter, model, optimizer, train_loader, \
+        scheduler, num_token_passed = init_from_checkpoint(checkpoint_iter=None)
+
+    loss_func = mint.nn.CrossEntropyLoss()
 
     def forward_fn(input_ids, attention_mask, labels):
         _, _, logits = model.construct(
@@ -69,13 +134,16 @@ def train():
 
     grad_fn = ms.value_and_grad(forward_fn, None, optimizer.parameters)
     lst_time = time()
+    sum_reduce = ops.AllReduce(op=ops.ReduceOp.SUM)
 
-    for iter, input_data in enumerate(train_loader):
+    for iter, input_data in enumerate(train_loader, start=start_iter):
         # prepare input
         input_ids = input_data['input_ids'][:, :-1]
         attention_mask = input_data['attention_mask'][:, :-1]
-        labels = input_data['input_ids'][:, 1:].copy().astype(ms.int32)
+        labels = input_data['input_ids'][:, 1:].copy().to(ms.int32)
         labels[input_data['attention_mask'][:, 1:] == 0] = -100
+
+        num_token_passed += sum_reduce(ops.sum(labels != -100).to(ms.int32)).item()
 
         # optim step
         with Timer(print_to_screen=False) as train_timer:
@@ -84,16 +152,28 @@ def train():
             optimizer.construct(grads)
             scheduler.step()
 
-        print('iter {} | lr: {:.1e} | loss: {:.3f} | aux_loss: {:.3f} | mem: {:.1f}GB/{:.1f}GB | time: {:.2f}s+{:.2f}s'.format(
-            iter, float(scheduler.get_lr()[0]), float(loss), float(model.aux_loss),
-            ms.runtime.max_memory_allocated() / (2**30),
-            ms.runtime.memory_allocated() / (2**30),
-            train_timer.elapsed_time,
-            time() - lst_time - train_timer.elapsed_time,
-        ), flush=True)
+        loss_to_print = sum_reduce(loss) / bms.world_size()
+        print(
+            f"iter {iter} | lr: {float(scheduler.get_lr()[0]):.1e} | "
+            f"loss: {float(loss_to_print):.3f} | aux_loss: {float(model.aux_loss):.3f} | "
+            f"#token passed: {num_token_passed:.2e} | "
+            f"mem: {ms.runtime.max_memory_allocated() / (2**30):.1f}GB/"
+            f"{ms.runtime.memory_allocated() / (2**30):.1f}GB | "
+            f"time: {train_timer.elapsed_time:.2f}s+"
+            f"{time() - lst_time - train_timer.elapsed_time:.2f}s",
+            flush=True
+        )
         lst_time = time()
 
-    #bms.save(model, MODELFILE_SAVE_PATH)
+        if (iter + 1) % 500 == 0:
+            with Timer('save_checkpoint'):
+                save_checkpoint(
+                    iter=iter,
+                    model=model,
+                    optimizer=optimizer,
+                    data_loader=train_loader,
+                    num_token_passed=num_token_passed,
+                )
 
 bms.init_distributed()
 
